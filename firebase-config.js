c3d9c9b0da2997f3816c700a4d6ee21ff2e61389
Jashwant Nukala <243741585+jashwantnukala@users.noreply.github.com>
2026-09-04 11:07:54 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3Lv_9EiVBVy5aXwiYOn0pgZu31ykFOGM",
+  authDomain: "digitalbellplisworkthistime.firebaseapp.com",
+  databaseURL: "https://digitalbellplisworkthistime-default-rtdb.firebaseio.com",
+  projectId: "digitalbellplisworkthistime",
+  storageBucket: "digitalbellplisworkthistime.firebasestorage.app",
+  messagingSenderId: "829189847707",
+  appId: "1:829189847707:web:99b13d5fe551b54d9ce43e",
+  measurementId: "G-W0PYZY1R1M"
+};
+
+// Initialize Firebase (CDN style)
+firebase.initializeApp(firebaseConfig);
+
